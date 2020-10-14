ec7ebd9a4fc4031c0f1ea37c15836f09b55fc75f
Ziah10 <saaron356@gmail.com>
2020-10-14 20:30:00 +0000

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,13 +3,14 @@ import { Http, Response } from "@angular/http";
 import "rxjs/add/operator/map";
 import { Observable } from "rxjs/Observable";
 import { Album } from "./album";
+import { Product } from './product';
+
 
 @Injectable()
 export class ProductService {
-  getProducts() {
-    throw new Error('Method not implemented.');
-  }
+  
   private _albumUrl = "../assets/album.json";
+  private _productsUrl = '../assets/products.json';
 
   constructor(private _http: Http) {}
 
@@ -18,4 +19,9 @@ export class ProductService {
       .get(this._albumUrl)
       .map((response) => <Album>response.json());
   }
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl).map(response =>
+    <Product[]>response.json());
+  }
+  
 }
